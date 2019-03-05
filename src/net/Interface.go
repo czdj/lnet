127c07c8f69490be510a816f144e6d667ab0c8c7
lvbin <binlv@123u.com>
2019-03-05 18:35:28 +0800

    添加新文件

--- src/net/Interface.go
+++ src/net/Interface.go
@@ -1,1 +1,30 @@
 package net
+
+
+type Transport interface {
+	Listen()
+	Read()
+	Write()
+	Close()
+}
+
+type DefTransport struct{
+	Net  string
+	Addr string
+}
+
+func (this *DefTransport) Listen(){
+
+}
+
+func (this *DefTransport) Read(){
+
+}
+
+func (this *DefTransport) Write(){
+
+}
+
+func (this *DefTransport) Close(){
+
+}
--- src/net/tcp.go
+++ src/net/tcp.go
@@ -1,1 +1,21 @@
 package net
+
+type TcpTransport struct {
+	DefTransport
+}
+
+func (this *TcpTransport) Listen(){
+
+}
+
+func (this *TcpTransport) Read(){
+
+}
+
+func (this *TcpTransport) Write(){
+
+}
+
+func (this *TcpTransport) Close(){
+
+}
